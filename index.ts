9063aa5dc59ff16b270145b66f7b62b875f252c3
Rajiv Giri <rajivgiri513@gmail.com>
2022-02-10 22:40:35 -0500

    Issue connecting to a Mongo DB Cluster

--- index.ts
+++ index.ts
@@ -7,19 +7,15 @@ import { ConnectionOptions } from 'tls';
 const app = express();
 const PORT: number = 3000;
 const mlabUser: string = 'root';
-const mlabPass: string = 'root';
+const mlabPass: string = 'RE6LwMSGGaKmp5Cm';
 
 const dataConnection = (user: string , pass: string): string => {
 
-return 'mongodb+srv://${user}:${pass}@cluster0.q34li.mongodb.net/linkedinDB?retryWrites=true&w=majority';
+return 'mongodb://${user}:${pass}@cluster0.q34li.mongodb.net/linkedinDB?retryWrites=true&w=majority';
 }
 
 let database = dataConnection(mlabUser, mlabPass);
-//Coinnection string
-// mongoose connection
-//Array Test
-//let simpleArray: number[] = [1,2,3,4];
-//mongoose.Promise = global.Promise;
+
 mongoose.connect(database, {
     useNewUrlParser: true,
   useUnifiedTopology: true,
